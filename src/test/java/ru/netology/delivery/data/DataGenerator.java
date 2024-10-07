3633be2b18571b4bfe32b40c93c4e58d776e55ee
Alexei <sepens@yandex.ru>
2024-10-07 17:29:44 +0300

    Initial commit

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,53 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String getMinDate(long plusDays) {
+        return LocalDate.now().plusDays(plusDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String getCity() {
+        String[] cities = new String[]{"Смоленск", "Уфа", "Тверь", "Санкт-Петербург", "Москва", "Пермь", "Калуга",
+                "Рязань", "Вологда"};
+        return cities[new Random().nextInt(cities.length - 1)];
+    }
+
+    public static String getName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String getPhoneNum() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserData getUser() {
+            return new UserData(getCity(), getName(), getPhoneNum());
+        }
+
+    }
+
+    @Value
+    public static class UserData {
+        String city;
+        String name;
+        String phone;
+    }
+
+
+}
---
+++ src/test/java/ru/netology/delivery/test/CardRequestTest.java
@@ -0,0 +1,47 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CardRequestTest {
+
+    long PLUS_DAYS = 3;
+    String APP_ADDRESS = "http://localhost:9999";
+
+    @Test
+    public void shouldSuccessfullySendRequest() throws InterruptedException {
+        Selenide.open(APP_ADDRESS);
+        $("[data-test-id='city'] input").setValue("Смоленск");
+        String inputDate = DataGenerator.getMinDate(PLUS_DAYS);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(inputDate);
+        $("[data-test-id='name'] input").setValue("Лягушеслав Болотин");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + inputDate));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        inputDate = DataGenerator.getMinDate(PLUS_DAYS + 1);
+        $("[data-test-id='date'] input").setValue(inputDate);
+        $("button.button").click();
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(visible);
+        Thread.sleep(3000);
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + inputDate));
+    }
+
+}
